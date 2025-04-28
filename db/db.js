cd015ad72e3b7ce6e55335f69f83854642249bbc
SURAJ-K-GUPTA <116446956+SURAJ-K-GUPTA@users.noreply.github.com>
2025-04-28 15:18:54 +0530

    `Removed hardcoded MongoDB URI from connectDB function`

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/supansha');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to MongoDB');
     } catch (error) {    
         console.error('MongoDB connection error:', error);
